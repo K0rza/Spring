d29778f480e9ac038bb19ca4ece4f88ada9aebe4
K0rza <h_enes_1997@hotmail.com>
2026-01-10 17:13:05 +0300

    spring hello world is added with rest controller

---
+++ p04-spring-hello-world/hello-world-spring/src/main/java/com/example/hello_world_spring/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.example.hello_world_spring.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // '/' karakteri girildiğinde "Hello World" yazdıracak
+
+    @GetMapping("/")
+    public String sayHello() { return "Hello World";}
+
+}
