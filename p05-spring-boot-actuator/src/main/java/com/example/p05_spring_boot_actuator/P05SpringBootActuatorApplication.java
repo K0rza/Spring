a815b7f7f86b90448efac6ef203b12696687dc58
K0rza <h_enes_1997@hotmail.com>
2026-01-11 00:10:04 +0300

    actuator sring boot starter project is added

---
+++ p05-spring-boot-actuator/src/main/java/com/example/p05_spring_boot_actuator/P05SpringBootActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.p05_spring_boot_actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P05SpringBootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P05SpringBootActuatorApplication.class, args);
+	}
+
+}
---
+++ p05-spring-boot-actuator/src/test/java/com/example/p05_spring_boot_actuator/P05SpringBootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p05_spring_boot_actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P05SpringBootActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
