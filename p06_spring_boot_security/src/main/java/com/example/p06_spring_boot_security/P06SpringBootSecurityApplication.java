85f5a23997680846d42b362f76b545daa03be6e6
K0rza <h_enes_1997@hotmail.com>
2026-01-11 00:44:21 +0300

    spring boot security starter project is added

---
+++ p06_spring_boot_security/src/main/java/com/example/p06_spring_boot_security/P06SpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.p06_spring_boot_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P06SpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P06SpringBootSecurityApplication.class, args);
+	}
+
+}
