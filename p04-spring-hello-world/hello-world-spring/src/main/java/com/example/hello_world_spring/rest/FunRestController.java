7627bfdfbbd169a8b7162349166b290ce8b65d4a
K0rza <h_enes_1997@hotmail.com>
2026-01-10 23:26:37 +0300

    workout get request is added

--- p04-spring-hello-world/hello-world-spring/src/main/java/com/example/hello_world_spring/rest/FunRestController.java
+++ p04-spring-hello-world/hello-world-spring/src/main/java/com/example/hello_world_spring/rest/FunRestController.java
@@ -3,12 +3,17 @@ package com.example.hello_world_spring.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class FunRestController {
 
     // '/' karakteri girildiğinde "Hello World" yazdıracak
 
     @GetMapping("/")
-    public String sayHello() { return "Test";}
+    public String sayHello() { return "Hello World";}
+
+    @GetMapping("/workout")
+    public String sayTheCongrats5km() {return "Congrats for 5km!";}
+    
 
 }
